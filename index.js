46bc3ff3e5934744e34376da94df72bc93859a8c
ketanjangid <ketanjangid786@gmail.com>
2023-11-18 20:28:29 +0530

    added jQuery and tested

---
+++ index.js
@@ -0,0 +1,9 @@
+$("button").click( () => console.log("button pressed"));
+
+function buttonResponse() {
+    // var buttonColor = $("button").css("background-color");
+    
+    // $("button").css("background-color", "grey");
+
+    // setTimeout(200, $("button").css("background-color", buttonColor));
+}
