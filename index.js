6ff6dcac7b6c35bdc50ca393d4b8f50af5e2eb04
ketanjangid <ketanjangid786@gmail.com>
2023-11-19 16:04:59 +0530

    added sounds and synced it to buttons

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-$("button").click( () => console.log("button pressed"));
+var buttonColors = ["green", "red", "yellow", "blue"];
+var gamePattern = [];
+
+
+$("button").click(buttonResponse);
 
 function buttonResponse() {
-    // var buttonColor = $("button").css("background-color");
-    
-    // $("button").css("background-color", "grey");
+    var buttonPressed = $(this).attr('id');
 
-    // setTimeout(200, $("button").css("background-color", buttonColor));
+    playSound(buttonPressed);
+    
+}
+
+function playSound(button) {
+    var sound = new Audio("sounds/" + button + ".mp3");
+
+    sound.play();
+}
+
+function nextSequence() {
+    let randomNum = Math.floor(4*Math.random());
+
+    let randomChosenColor = buttonColors[randomNum];
+
+    gamePattern.push(randomChosenColor);
 }
